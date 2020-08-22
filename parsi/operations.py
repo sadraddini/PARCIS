c55b457e6d9d710ed2aaadebd1eeb6b959f47508
Kasra Ghasemi <kasra0gh@bu.edu>
2020-08-22 15:12:16 -0400

    Adding objective function to optimization

--- parsi/operations.py
+++ parsi/operations.py
@@ -10,9 +10,8 @@ except:
 try:
     import pydrake.solvers.mathematicalprogram as MP
     import pydrake.solvers.gurobi as Gurobi_drake
-    import pydrake.solvers.osqp as OSQP_drake
     # use Gurobi solver
-    global gurobi_solver,OSQP_solver, license
+    global gurobi_solver, license
     gurobi_solver=Gurobi_drake.GurobiSolver()
     license = gurobi_solver.AcquireLicense()
 except:
@@ -22,7 +21,7 @@ try:
 except:
     warnings.warn("You don't have parsi not properly installed.")
 
-def rci(system,order_max=10,size='min'):
+def rci(system,order_max=10,size='min',obj='include_center'):
     """
     Given a LTI system, this function returns a rci set and its action set.
     """
@@ -32,13 +31,23 @@ def rci(system,order_max=10,size='min'):
         print('order',order)
         prog=MP.MathematicalProgram()
         var=parsi.rci_constraints(prog,system,T_order=order)
+        T=var['T'].flatten()
 
         #Defining the objective function
-        # if size=='max':
+        #objective function for minimizing the size of the RCI set
+        if size=='min':
+            prog.AddQuadraticErrorCost(
+            Q=np.eye(len(T)),
+            x_desired=np.zeros(len(T)),
+            vars=T)
+        #objective function for minimizing the distance between the RCI set and the set of admissible states
+        if obj=='include_center':
+            prog.AddQuadraticErrorCost(
+            Q=np.eye(len(var['T_x'])) if size=='min' else -1*np.eye(len(T)),
+            x_desired=system.X.x,
+            vars=var['T_x'])
 
-        # else:
-
-
+        #Result
         result=gurobi_solver.Solve(prog)    
         print('result',result.is_success())
         beta = system.beta if not 'beta' in var else result.GetSolution(var['beta'])
